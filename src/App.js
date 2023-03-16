25155662b4176b92b7fbdd382851193aef1ce0ec
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-16 23:53:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = 0
-const image = 'https://pbs.twimg.com/profile_images/1632558122442031106/56U4BY_e_400x400.jpg'
-const Title = 'Pablo Escobar Punks'
-const supply = 250
+const ethAmount = '0 / 0.0025'
+const image = 'https://pbs.twimg.com/profile_images/1636346076176195584/qRrA9l6y_400x400.png'
+const Title = 'GURBUS.LAB'
+const supply = 1571
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
