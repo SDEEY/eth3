204e09fb32437f08d4e873bc16641a3646fec9c3
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-28 22:41:56 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.069'
-const image = 'https://pbs.twimg.com/profile_images/1611031536937099267/78NzTx61_400x400.jpg'
-const Title = 'Viktor NFT.NYC 2023'
-const supply = 977
+const ethAmount = '0.01'
+const image = 'https://pbs.twimg.com/profile_images/1542157554314272772/CqN_aYWH_400x400.png'
+const Title = 'Bored Yachts'
+const supply = 72
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
