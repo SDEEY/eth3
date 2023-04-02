e430b4cb9d4f5bcab82360bc95d9ecbd8f30e0b3
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-02 22:53:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
 const ethAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1593377865445396492/fOdaWmUl_400x400.jpg'
-const Title = 'Dead Degens'
-const supply = 799
+const image = 'https://pbs.twimg.com/profile_images/1611388522857906181/lIUUqp71_400x400.jpg'
+const Title = 'Andy Prinz'
+const supply = 1000
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
