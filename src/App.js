51d7b29590ab1e5c65fbe456f632fd2c6672dcb4
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-26 20:49:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.04'
-const image = 'https://cdn.bio.link/uploads/profile_pictures/2023-01-24/dj4qhMboSyKKZo1Czi5unNucpBippDQi.png'
-const Title = 'WolfPunX'
-const supply = 500
+const ethAmount = '0.21'
+const image = 'https://pbs.twimg.com/profile_images/1577450157217525760/rZqsWD_b_400x400.jpg'
+const Title = 'Crypto Cuisines Club'
+const supply = 111
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
