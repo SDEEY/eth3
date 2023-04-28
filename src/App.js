bf5bf1f47891aa8ee56805de98c16650f00b973d
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-28 17:51:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.02'
-const image = 'https://pbs.twimg.com/profile_images/1637653231110324225/SLFNT29T_400x400.jpg'
-const Title = 'ZkSync Era'
-const supply = 2000
+const ethAmount = '0.2 / 0.35'
+const image = 'https://pbs.twimg.com/profile_images/1623786655915163649/tJzHGpaM_400x400.jpg'
+const Title = 'DrPunksNFT'
+const supply = 555
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
