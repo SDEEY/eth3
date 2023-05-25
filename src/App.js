85e244f2c3c51d410e6c63af5018563b58e2af1e
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-25 20:11:51 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.02'
-const image = 'https://pbs.twimg.com/profile_images/1455566202542034954/dVxfZtGd_400x400.jpg'
-const Title = 'Parasites'
-const supply = 10000
+const ethAmount = '0'
+const image = 'https://pbs.twimg.com/profile_images/1557391866106183681/l0vlBC23_400x400.jpg'
+const Title = 'Peace Eagle NFT'
+const supply = 3300
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
