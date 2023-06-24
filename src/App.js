4d2e325fc421e776ab21292bef6f0e68e0845114
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-06-24 16:51:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.018'
-const image = 'https://pbs.twimg.com/profile_images/1616831933584924685/8CqNqpiM_400x400.jpg'
-const Title = 'audiogalleries'
-const supply = 333
+const ethAmount = '0.028'
+const image = 'https://pbs.twimg.com/profile_images/1671136338093957122/-gUOCMWU_400x400.jpg'
+const Title = 'Krunk'
+const supply = 1994
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
