6baa8d339cb5b534d6a58f908a157a444b4a3b44
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-07-22 01:51:12 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0'
-const image = 'https://pbs.twimg.com/profile_images/1678127209431826433/xWz4kVMU_400x400.jpg'
-const Title = 'Robo Elementals'
-const supply = 971
+const ethAmount = '0.15'
+const image = 'https://pbs.twimg.com/profile_images/1661702808243146764/uFHsqpm2_400x400.jpg'
+const Title = 'HERE & NOW'
+const supply = 71
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
